c6a8421c6710ecc68e3e5aa18552a97a4b804c82
Homin Lee <homin.lee@suapapa.net>
2013-04-11 18:47:00 +0900

    encoding/cp949: Add support for Korean native encoding, cp949

---
+++ encoding/cp949/cp949.go
@@ -0,0 +1,266 @@
+package cp949
+
+import (
+	"bytes"
+	"encoding/binary"
+	"sort"
+	"sync"
+	"unicode/utf8"
+)
+
+var (
+	fromLookupMutex sync.Mutex
+	fromLookupTable cp949Table
+
+	toLookupMutex sync.Mutex
+	toLookupTable cp949Table
+)
+
+// code pair for a Korean chracter
+type cp949Code struct {
+	native  uint16 // cp949
+	unicode rune   // ucs4
+}
+
+// lookup table for translator
+type cp949Table []cp949Code
+
+func (t cp949Table) Len() int {
+	return len(t)
+}
+
+func (t cp949Table) Swap(i, j int) {
+	t[i], t[j] = t[j], t[i]
+}
+
+// instance type to sort the lookup table by native code for from-translator
+type cp949TableSortByNative struct{ cp949Table }
+
+func (t cp949TableSortByNative) Less(i, j int) bool {
+	return t.cp949Table[i].native < t.cp949Table[j].native
+}
+
+// instance type to sort the lookup table by unicode for to-translator
+type cp949TableSortByUnicode struct{ cp949Table }
+
+func (t cp949TableSortByUnicode) Less(i, j int) bool {
+	return t.cp949Table[i].unicode < t.cp949Table[j].unicode
+}
+
+type translator interface {
+	Translate(data []byte) (int, []byte, error)
+}
+
+// use same struct to from-translator and to-translator.
+// each translators use sort.Search() to find corresponding code.
+// And, the lookup table is sorted by a filed which have
+// same encoding of input data.
+type translateCp949 struct {
+	table   cp949Table // lookup table
+	scratch []byte     // buffer for output
+}
+
+// from cp949 to unicode translator
+type translateFromCp949 translateCp949
+
+func (p *translateFromCp949) Translate(data []byte) (int, []byte, error) {
+	p.scratch = p.scratch[:0]
+	c := 0
+	for len(data) > 0 {
+		if data[0]&0x80 == 0 {
+			p.scratch = append(p.scratch, data[0])
+			data = data[1:]
+			c += 1
+			continue
+		}
+
+		n := uint16(data[0])<<8 | uint16(data[1])
+		fi := sort.Search(len(p.table), func(i int) bool {
+			if n <= p.table[i].native {
+				return true
+			}
+			return false
+		})
+
+		f := p.table[fi]
+		if n == f.native {
+			p.scratch = appendRune(p.scratch, f.unicode)
+		} else {
+			p.scratch = appendRune(p.scratch, utf8.RuneError)
+		}
+		data = data[2:]
+		c += 2
+	}
+	return c, p.scratch, nil
+}
+
+// from unicode to cp949 translator
+type translateToCp949 translateCp949
+
+func (p *translateToCp949) Translate(data []byte) (int, []byte, error) {
+	p.scratch = p.scratch[:0]
+	c := 0
+	for len(data) > 0 {
+		if data[0]&0x80 == 0 {
+			p.scratch = append(p.scratch, data[0])
+			data = data[1:]
+			c += 1
+			continue
+		}
+
+		r, s := utf8.DecodeRune(data)
+		fi := sort.Search(len(p.table), func(i int) bool {
+			if r <= p.table[i].unicode {
+				return true
+			}
+			return false
+		})
+
+		if fi >= len(p.table) {
+			p.scratch = append(p.scratch, '?')
+		}
+
+		if fi < len(p.table) && r == p.table[fi].unicode {
+			f := p.table[fi]
+			p.scratch = append(p.scratch,
+				byte(f.native>>8), byte(f.native&0xff))
+		} else {
+			p.scratch = append(p.scratch, '?')
+		}
+
+		data = data[s:]
+		c += s
+	}
+	return c, p.scratch, nil
+}
+
+// load cp949.dat to cp949Table
+func loadCp949Table() (cp949Table, error) {
+	buf := bytes.NewBufferString(cp949LookupData)
+
+	// read info header
+	var datInfo struct {
+		CodeCnt, ChunkCnt uint16
+	}
+	if err := binary.Read(buf, binary.BigEndian, &datInfo); err != nil {
+		return nil, err
+	}
+
+	// read code chunks to table
+	table := make(cp949Table, datInfo.CodeCnt)
+	table = table[:0]
+	var chunk struct {
+		Code, Len uint16
+	}
+	for i := uint16(0); i < datInfo.ChunkCnt; i++ {
+		if err := binary.Read(buf, binary.BigEndian, &chunk); err != nil {
+			return nil, err
+		}
+
+		line := make([]byte, chunk.Len)
+		if n, err := buf.Read(line); n != int(chunk.Len) || err != nil {
+			return nil, err
+		}
+
+		for _, u := range string(line) {
+			table = append(table,
+				cp949Code{native: chunk.Code, unicode: u})
+			chunk.Code += 1
+		}
+	}
+
+	return table, nil
+}
+
+func loadFromCp949LookupTable() (cp949Table, error) {
+	fromLookupMutex.Lock()
+	defer fromLookupMutex.Unlock()
+
+	if fromLookupTable != nil {
+		return fromLookupTable, nil
+	}
+
+	t, err := loadCp949Table()
+	if err != nil {
+		return nil, err
+	}
+	if !sort.IsSorted(cp949TableSortByNative{t}) {
+		panic("cp949.dat is not sorted by native code!")
+	}
+
+	fromLookupTable = t
+	return fromLookupTable, nil
+}
+
+func loadToCp949LookupTable() (cp949Table, error) {
+	toLookupMutex.Lock()
+	defer toLookupMutex.Unlock()
+
+	if toLookupTable != nil {
+		return toLookupTable, nil
+	}
+
+	ft, err := loadFromCp949LookupTable()
+	if err != nil {
+		return nil, err
+	}
+
+	t := make(cp949Table, len(ft))
+	copy(t, ft)
+	sort.Sort(cp949TableSortByUnicode{t})
+
+	toLookupTable = t
+	return toLookupTable, nil
+}
+
+func fromCp949() (translator, error) {
+	t, err := loadFromCp949LookupTable()
+	if err != nil {
+		return nil, err
+	}
+	return &translateFromCp949{table: t}, nil
+}
+
+func toCp949() (translator, error) {
+	t, err := loadToCp949LookupTable()
+	if err != nil {
+		return nil, err
+	}
+	return &translateToCp949{table: t}, nil
+}
+
+// ensureCap returns s with a capacity of at least n bytes.
+// If cap(s) < n, then it returns a new copy of s with the
+// required capacity.
+func ensureCap(s []byte, n int) []byte {
+	if n <= cap(s) {
+		return s
+	}
+	// logic adapted from appendslice1 in runtime
+	m := cap(s)
+	if m == 0 {
+		m = n
+	} else {
+		for {
+			if m < 1024 {
+				m += m
+			} else {
+				m += m / 4
+			}
+			if m >= n {
+				break
+			}
+		}
+	}
+	t := make([]byte, len(s), m)
+	copy(t, s)
+	return t
+}
+
+// XXX: can it be simpler?
+func appendRune(buf []byte, r rune) []byte {
+	n := len(buf)
+	buf = ensureCap(buf, n+utf8.UTFMax)
+	nu := utf8.EncodeRune(buf[n:n+utf8.UTFMax], r)
+	return buf[0 : n+nu]
+}
---
+++ encoding/cp949/public.go
@@ -0,0 +1,129 @@
+package cp949
+
+import (
+	"io"
+)
+
+// convert utf-8 stream to cp949 stream
+func From(in []byte) ([]byte, error) {
+	tr, err := fromCp949()
+	if err != nil {
+		return nil, err
+	}
+
+	_, out, err := tr.Translate(in)
+	if err != nil {
+		return nil, err
+	}
+
+	return out, nil
+}
+
+// convert cp949 stream to utf-8 stream
+func To(in []byte) ([]byte, error) {
+	tr, err := toCp949()
+	if err != nil {
+		return nil, err
+	}
+
+	_, out, err := tr.Translate(in)
+	if err != nil {
+		return nil, err
+	}
+
+	return out, nil
+}
+
+type cp949Reader struct {
+	r     io.Reader
+	tr    translator
+	cdata []byte // unconsumed data from converter.
+	rdata []byte // unconverted data from reader.
+	err   error  // final error from reader.
+}
+
+// create io.Reader which read cp949 source to utf-8
+func NewReader(r io.Reader) (io.Reader, error) {
+	tr, err := fromCp949()
+	if err != nil {
+		return nil, err
+	}
+
+	return &cp949Reader{r: r, tr: tr}, nil
+}
+
+func (r *cp949Reader) Read(buf []byte) (int, error) {
+	for {
+		if len(r.cdata) > 0 {
+			n := copy(buf, r.cdata)
+			r.cdata = r.cdata[n:]
+			return n, nil
+		}
+		if r.err == nil {
+			r.rdata = ensureCap(r.rdata, len(r.rdata)+len(buf))
+			n, err := r.r.Read(r.rdata[len(r.rdata):cap(r.rdata)])
+			// Guard against non-compliant Readers.
+			if n == 0 && err == nil {
+				err = io.EOF
+			}
+			r.rdata = r.rdata[0 : len(r.rdata)+n]
+			r.err = err
+		} else if len(r.rdata) == 0 {
+			break
+		}
+		nc, cdata, cvterr := r.tr.Translate(r.rdata)
+		if cvterr != nil {
+			// TODO
+		}
+		r.cdata = cdata
+
+		// Ensure that we consume all bytes at eof
+		// if the converter refuses them.
+		if nc == 0 && r.err != nil {
+			nc = len(r.rdata)
+		}
+
+		// Copy unconsumed data to the start of the rdata buffer.
+		r.rdata = r.rdata[0:copy(r.rdata, r.rdata[nc:])]
+	}
+	return 0, r.err
+}
+
+type cp949Writer struct {
+	w   io.Writer
+	tr  translator
+	buf []byte // unconsumed data from writer.
+}
+
+// creater io.Writer which write utf8 input src to cp949
+func NewWriter(w io.Writer) (io.Writer, error) {
+	tr, err := toCp949()
+	if err != nil {
+		return nil, err
+	}
+
+	return &cp949Writer{w: w, tr: tr}, nil
+}
+
+func (w *cp949Writer) Write(data []byte) (int, error) {
+	wdata := data
+	if len(w.buf) > 0 {
+		w.buf = append(w.buf, data...)
+		wdata = w.buf
+	}
+	n, cdata, err := w.tr.Translate(wdata)
+	if err != nil {
+		// TODO
+	}
+	if n > 0 {
+		_, err = w.w.Write(cdata)
+		if err != nil {
+			return 0, err
+		}
+	}
+	w.buf = w.buf[:0]
+	if n < len(wdata) {
+		w.buf = append(w.buf, wdata[n:]...)
+	}
+	return len(data), nil
+}
