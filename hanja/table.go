8634ec0542d4d62600242d8bd7b2cdcde599a38f
Homin Lee <homin.lee@suapapa.net>
2013-05-10 10:08:02 +0900

    hanja: add license header

--- hanja/table.go
+++ hanja/table.go
@@ -1,3 +1,7 @@
+// Copyright 2013, Homin Lee <homin.lee@suapapa.net>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package hanja
 
 var (
