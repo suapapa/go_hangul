d1332fa5e7b46f67dfb8b7cb02b4217b5bfabeea
Homin Lee <homin.lee@suapapa.net>
2015-10-08 09:38:37 +0900

    Add license header in doc.go

--- encoding/cp949/doc.go
+++ encoding/cp949/doc.go
@@ -1,3 +1,7 @@
+// Copyright 2013-2015, Homin Lee. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Package cp949 provide support for CP949 character encoding,
 // It's degault encoding in Korean MS Windows. *sigh*
 package cp949
